746a44215d5177f86c6721ca3b46e2b325babde8
wangmaosong@oceanum.com <lygr@0907>
2022-05-03 20:05:35 +0800

    feat：区分缓存策略

--- simba-auth/src/main/java/com/wxf/simba/auth/service/impl/SysUserServiceImpl.java
+++ simba-auth/src/main/java/com/wxf/simba/auth/service/impl/SysUserServiceImpl.java
@@ -6,6 +6,7 @@ import com.wxf.simba.auth.mapper.SysUserMapper;
 import com.wxf.simba.auth.service.SysUserService;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,7 +49,7 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> impl
         return this.baseMapper.selectList(null);
     }
 
-    @CachePut(cacheNames = "sys_user", key = "#id")
+    @Cacheable(cacheNames = "sys_user", key = "#id")
     @Override
     public SysUser getSysUserById(String id) {
         return this.baseMapper.selectById(id);
