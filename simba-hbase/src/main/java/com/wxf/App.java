2a50f7c04c50ddc682991d02799a1c3aa1baad9e
王小凡 <390871264@qq.com>
2022-06-12 10:49:04 +0800

    feat: add grpc model && python test

--- simba-hbase/src/main/java/com/wxf/App.java
+++ simba-hbase/src/main/java/com/wxf/App.java
@@ -1,10 +1,15 @@
 package com.wxf;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        System.out.println(list.stream().count());
+
     }
 }
---
+++ simba-python/com/wxf/dist_test.py
@@ -0,0 +1,8 @@
+
+
+# for 循环
+person = {'name': 'wms', 'age': 20}
+for k, v in person.items():
+    print(k + ": " + v)
+
+print(person.values())
