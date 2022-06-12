e9d66473369834cc165873bebdc5d101f363db52
王小凡 <390871264@qq.com>
2022-06-12 20:50:51 +0800

    feat: list

---
+++ simba-python/com/wxf/list_test.py
@@ -0,0 +1,22 @@
+# 初始化
+list1 = list(range(1, 10))
+
+print(list1.pop())
+
+print(list1)
+
+list1.reverse()
+
+print(list1)
+
+list1.sort()
+
+print(list1)
+
+list1.append(1)
+list1.insert(0, 1)
+print(list1.count(1))
+
+print(list1.index(1))
+
+print(list1.copy())
