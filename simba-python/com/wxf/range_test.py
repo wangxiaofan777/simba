84a1c71df84bc227a43dc5dbda9443f4b971acb9
王小凡 <390871264@qq.com>
2022-06-12 12:17:21 +0800

    feat: range

---
+++ simba-python/com/wxf/range_test.py
@@ -0,0 +1,18 @@
+
+
+# 遍历
+for i in range(10):
+    print(i)
+
+# 增幅
+print(list(range(0, 10, 3)))
+
+print(list(range(0, -100, -30)))
+
+# 求和
+print(sum(range(10)))
+
+
+array = ['Hello', 'World']
+for a in range(len(array)):
+    print(a, array[a])
