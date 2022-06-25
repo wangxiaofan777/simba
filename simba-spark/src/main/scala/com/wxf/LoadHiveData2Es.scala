f07e131adf0f7f7f1cdba167b33204ece58ccbdd
王小凡 <390871264@qq.com>
2022-06-25 12:33:57 +0800

    feat: 测试

--- simba-spark/src/main/scala/com/wxf/LoadHiveData2Es.scala
+++ simba-spark/src/main/scala/com/wxf/LoadHiveData2Es.scala
@@ -1,7 +1,8 @@
 package com.wxf
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SparkSession}
 import org.elasticsearch.spark.rdd.EsSpark
 
@@ -14,34 +15,38 @@ object LoadHiveData2Es {
     conf.set("es.index.auto.create", "true")
     val session: SparkSession = SparkSession.builder().config(conf).appName("LoadHiveData2Es").master("yarn").enableHiveSupport().getOrCreate()
     val javaRDD: JavaRDD[Row] = session.sql("insert into wms.tablea select * from cmk.tablea;").toJavaRDD
-    val rdd: JavaRDD[(String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String)] = javaRDD.map(x => {
-      Tuple22(
-        x.getAs[String]("phone"),
-        x.getAs[String]("dt"),
-        x.getAs[String]("a1"),
-        x.getAs[String]("a2"),
-        x.getAs[String]("a3"),
-        x.getAs[String]("a4"),
-        x.getAs[String]("a5"),
-        x.getAs[String]("a6"),
-        x.getAs[String]("a7"),
-        x.getAs[String]("a8"),
-        x.getAs[String]("a9"),
-        x.getAs[String]("a10"),
-        x.getAs[String]("a11"),
-        x.getAs[String]("a12"),
-        x.getAs[String]("a13"),
-        x.getAs[String]("a14"),
-        x.getAs[String]("a15"),
-        x.getAs[String]("a16"),
-        x.getAs[String]("a17"),
-        x.getAs[String]("a18"),
-        x.getAs[String]("a19"),
-        x.getAs[String]("a20")
+    val rdd: JavaRDD[Map[String, String]] = javaRDD.map(x => {
+      Map(
+        "phone" -> x.getAs[String]("phone"),
+        "dt" -> x.getAs[String]("dt"),
+        "a1" -> x.getAs[String]("a1"),
+        "a2" -> x.getAs[String]("a2"),
+        "a3" -> x.getAs[String]("a3"),
+        "a4" -> x.getAs[String]("a4"),
+        "a5" -> x.getAs[String]("a5"),
+        "a6" -> x.getAs[String]("a6"),
+        "a7" -> x.getAs[String]("a7"),
+        "a8" -> x.getAs[String]("a8"),
+        "a9" -> x.getAs[String]("a9"),
+        "a10" -> x.getAs[String]("a10"),
+        "a11" -> x.getAs[String]("a11"),
+        "a12" -> x.getAs[String]("a12"),
+        "a13" -> x.getAs[String]("a13"),
+        "a14" -> x.getAs[String]("a14"),
+        "a15" -> x.getAs[String]("a15"),
+        "a16" -> x.getAs[String]("a16"),
+        "a17" -> x.getAs[String]("a17"),
+        "a18" -> x.getAs[String]("a18"),
+        "a19" -> x.getAs[String]("a19"),
+        "a20" -> x.getAs[String]("a20")
       )
     })
+    val sc: SparkContext = session.sparkContext
+    val value: RDD[JavaRDD[Map[String, String]]] = sc.makeRDD(
+      Seq(rdd)
+    )
 
-    EsSpark.saveToEs(rdd, "tablea");
+    EsSpark.saveToEs(value, "tablea");
 
     session.stop()
   }
