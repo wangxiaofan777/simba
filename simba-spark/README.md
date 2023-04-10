4a9942b2f78c57eb803187c3059a9e8d5875223b
wangmaosong <wangmaosong@oceanum.cn>
2023-04-10 10:35:40 +0800

    feat: 读取CSV到mongo

--- simba-spark/README.md
+++ simba-spark/README.md
@@ -19,3 +19,22 @@ spark3-submit --class com.wxf.LoadHiveData2Es \
               --name LoadHiveData2Es \
               /home/wms/simba-spark-1.0-SNAPSHOT.jar
 ```
+
+
+
+```shell
+spark3-submit --class com.wxf.LoadHive2Mongo \
+              --master yarn  \
+              --deploy-mode cluster  \
+              --principal admin/admin@HADOOP.COM \
+              --keytab  /root/admin.keytab \
+              --name LoadHive2Mongo \
+              --driver-memory 4g \
+              --executor-memory 4g \
+              --driver-cores 2 \
+              --executor-cores 4 \
+              --queue root.users.admin \
+              --num-executors 1 \
+              /home/wms/simba-spark-1.0-SNAPSHOT.jar ads.variable_extraction_1
+              
+```
---
+++ simba-spark/src/main/scala/com/wxf/LoadHive2Mongo.scala
@@ -0,0 +1,28 @@
+package com.wxf
+
+import org.apache.spark.sql.SparkSession
+
+object LoadHive2Mongo {
+
+  def main(args: Array[String]): Unit = {
+
+    val tableName = "ads.variable_extraction_1"
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("MongoSparkConnectorIntro")
+      .config("spark.mongodb.read.connection.uri", "mongodb://admin:admin@10.50.17.191:29000/wms.wms?authSource=admin")
+      .config("spark.mongodb.write.connection.uri", "mongodb://admin:admin@10.50.17.191:29000/wms.wms?authSource=admin")
+      .enableHiveSupport()
+      .getOrCreate()
+
+
+    val df = spark.sql(s"select * from $tableName")
+
+    df.show()
+
+    df.write.format("mongodb").mode("overwrite").save()
+
+    spark.stop()
+  }
+
+}
